b1bdc68144b731ffce6a73912d1bb1e7e8636cfd
NakulChauhan1 <48352040+NakulChauhan1@users.noreply.github.com>
2020-09-14 16:26:01 +0530

    Commit

---
+++ STM32/stm32f4xx_drivers/Debug/drivers/Src/subdir.mk
@@ -0,0 +1,34 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/Src/gpio_driver.c \
+../drivers/Src/i2c_driver.c \
+../drivers/Src/spi_driver.c \
+../drivers/Src/usart_driver.c 
+
+OBJS += \
+./drivers/Src/gpio_driver.o \
+./drivers/Src/i2c_driver.o \
+./drivers/Src/spi_driver.o \
+./drivers/Src/usart_driver.o 
+
+C_DEPS += \
+./drivers/Src/gpio_driver.d \
+./drivers/Src/i2c_driver.d \
+./drivers/Src/spi_driver.d \
+./drivers/Src/usart_driver.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/Src/gpio_driver.o: ../drivers/Src/gpio_driver.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"N:/STM32/stm32f4xx_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"drivers/Src/gpio_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+drivers/Src/i2c_driver.o: ../drivers/Src/i2c_driver.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"N:/STM32/stm32f4xx_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"drivers/Src/i2c_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+drivers/Src/spi_driver.o: ../drivers/Src/spi_driver.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"N:/STM32/stm32f4xx_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"drivers/Src/spi_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+drivers/Src/usart_driver.o: ../drivers/Src/usart_driver.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DSTM32 -DSTM32F4 -DSTM32F446RETx -DDEBUG -DNUCLEO_F446RE -c -I../Inc -I"N:/STM32/stm32f4xx_drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"drivers/Src/usart_driver.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
